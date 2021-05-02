63e60891b1fe4a2138eff857da64ed97e8c70e30
Rhenica <83538157+Rhenica@users.noreply.github.com>
2021-05-02 18:46:42 +0300

    Update AntiCheat.java

--- src/com/acbase/AntiCheat.java
+++ src/com/acbase/AntiCheat.java
@@ -1,12 +1,16 @@
 package com.acbase;
 
+import com.acbase.check.CheckManager;
 import com.acbase.event.EventListener;
+import com.acbase.event.PacketListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 
 public final class AntiCheat extends JavaPlugin {
     private static AntiCheat instance;
+    private static CheckManager checkManager;
     public static String alertPerm = "anticheat.alerts";
     public static String prefix = "&8[&2AntiCheat&8] &7";
     public static String pc = "&7";
@@ -16,6 +20,9 @@ public final class AntiCheat extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        checkManager = new CheckManager();
+        Bukkit.getPluginManager().registerEvents(checkManager, instance);
+        Bukkit.getPluginManager().registerEvents(new PacketListener(), instance);
     }
 
     @Override
@@ -25,5 +32,9 @@ public final class AntiCheat extends JavaPlugin {
 
     public static AntiCheat getInstance() {
         return instance;
+    }
+
+    public static CheckManager getCheckManager() {
+        return checkManager;
     }
 }
